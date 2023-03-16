1f283e646cf50e9b3e5547b8e2ccdebae9a5d23e
Ajaythota <ajaykumarthota@yahoo.com>
2023-03-16 15:50:34 +0530

    added lines on the page

--- PDF_Generator.py
+++ PDF_Generator.py
@@ -7,14 +7,26 @@ for index, row in df.iterrows():
     pdf.add_page()
   #se the header
     pdf.set_font(family="times", style="B", size=12)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    for y in range(20, 240, 10): #lines on the page
+        pdf.line(10, y, 200, y)
     #set the footer
-    pdf.ln(265)
-    pdf.set_font(family="times", style="B", size=12)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1, border=1)
+    pdf.ln(240)
+    pdf.set_font(family="times", style="I", size=8)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
     for i in range(row["Pages"] -1):
         pdf.add_page()
+        # set the header
+        pdf.set_font(family="times", style="B", size=12)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        for y in range(20, 240, 10):  #lines on the page
+            pdf.line(10, y, 200, y)
+
+        # set the footer
+        pdf.ln(240)
+        pdf.set_font(family="times", style="I", size=8)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
 pdf.output("files/output.pdf")
 
