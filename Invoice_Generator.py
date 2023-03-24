9aeb25d0615d1db2773fa2925c70229d4fd6d460
Ajaythota <ajaykumarthota@yahoo.com>
2023-03-24 18:02:42 +0530

    adding new code

--- Invoice_Generator.py
+++ Invoice_Generator.py
@@ -15,6 +15,9 @@ ofile ="invoice/"+f1+".pdf"
 print(ofile)
 df_x = pd.read_excel(file,sheet_name="Sheet 1")
 print(df_x)
+
+total=str(df_x['total_price'].sum())
+print(total)
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 invoice= f"Invoice:{inv[0]}"
 date=f"Date:{inv[1]}"
@@ -38,4 +41,16 @@ for index, row in df_x.iterrows():
     pdf.cell(w=40, h=12, txt=str(row["price_per_unit"]), align="L", border=1)
     pdf.cell(w=40, h=12, txt=str(row["total_price"]), align="L", border=1,ln=1)
 
+pdf.set_font(family="times", style="B", size=12)
+pdf.cell(w=40, h=12, txt="", align="L", border=1)
+pdf.cell(w=40, h=12, txt="",align="L", border=1)
+pdf.cell(w=40, h=12, txt="", align="L", border=1)
+pdf.cell(w=40, h=12, txt="",align="L", border=1)
+pdf.cell(w=40, h=12, txt=total, align="L", border=1, ln=3)
+
+pdf.set_font(family="times", style="B", size=12)
+pdf.cell(w=35, h=12, txt=f"Total amount due is {total} Euros", align="L",  ln=1)
+pdf.set_font(family="times", size=10)
+pdf.cell(w=25, h=8, txt="pythonhow" )
+pdf.image("images/pythonhow.png",wdith=8)
 pdf.output(ofile)
